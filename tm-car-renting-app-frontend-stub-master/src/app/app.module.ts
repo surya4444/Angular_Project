a1c0cf4c35314b009a9ad1ec634a98b1fb1e0f5b
surya444 <gudusuryateja@gmail.com>
2020-09-11 14:39:26 +0530

    Checkpoint 1

--- tm-car-renting-app-frontend-stub-master/src/app/app.module.ts
+++ tm-car-renting-app-frontend-stub-master/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { SignInComponent } from './shared/sign-in/sign-in.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SignInComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
